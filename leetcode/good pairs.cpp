ec04883d6a4246a9197247c1eee96bc56fad39bf
shubhamg2205 <56907365+shubhamg2205@users.noreply.github.com>
2020-10-02 11:28:43 +0530

    added shufflestring

--- leetcode/good pairs.cpp
+++ leetcode/good pairs.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int> a) {
-        int ans=0;
+        int s=0;
             sort(a.begin(),a.end());
             for(int i=0;i<a.size()-1;i++){
                     if(a[i]!=a[i+1]) continue;
                     else{
                             int j=i+1;
                             while(j<a.size()){
-                               if(a[i]==a[j]) ans++;
+                               if(a[i]==a[j]) s++;
                                    else break;
                                     j++;
                             }
                     }
             }
-            return ans;
+            return s;
     }
 };
